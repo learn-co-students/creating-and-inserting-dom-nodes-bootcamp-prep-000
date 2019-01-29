594f88252e2d5a3eee327b035908490041cc0a2b
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-29 02:58:44 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+var element = document.createElement('div');
+element.innerHTML = 'Im a savage!';
+element.style.backgroundColor = '#f9f9f9';
+element.style.textAlign = `center`;
+document.body.appendChild(element);
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.style.textAlign = `left`;
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
+
+ul.removeChild(ul.querySelector(`li:nth-child(2)`));

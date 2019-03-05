dda789afe5c8645d72000f2fb9c9c6f81c25438f
Adam Kearney <aklives@hotmail.com>
2019-03-05 02:27:19 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,12 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+var ul = document.createElement('ul');
+
+element.innerHTML = 'Hello, DOM!';
+document.body.appendChild(element);
+
+for(let i = 0 ; i<3;i++){let li = document.createElement('li'); li.innerHTML = (i + 1).toString(); ul.appendChild(li);}
+
+document.body.appendChild(ul);
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+

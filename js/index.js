c4a0ede434e50286f629d585c67ab2147d6f230d
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-10 20:54:15 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -7,5 +7,40 @@ document.body.appendChild(element);
 
 element.style.textAlign = "center"; 
 
+//---
 
 var ul = document.createElement("ul"); 
+
+for (i=0; i<3; i++){
+  let li = document.createElement("li"); 
+  li.innerHTML = (i + 1).toString(); 
+  ul.appendChild(li)
+}
+
+element.appendChild(ul); 
+
+ul.style.textAlign = "center"; 
+
+//----
+
+ul.removeChild(ul.querySelector("li:nth-child(2)"));
+
+//----
+
+ul.remove(); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

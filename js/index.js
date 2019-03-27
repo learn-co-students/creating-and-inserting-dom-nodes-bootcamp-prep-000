3160d8e232b9fed2376dd266baaf1423a5f2505b
Carly Pecora <carlyannpecora@gmail.com>
2019-03-27 20:31:10 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+var div = document.createElement("div")
+div.style.background = "red"
+div.style.textAlign = "center"
+div.style.fontSize = "50px"
+div.innerHTML = "Hello, DOM"
+
+document.body.appendChild(div)
+
+var ul = document.createElement("ul")
+
+for( var i = 0; i < 5; i++){
+  let li = document.createElement("li")
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+div.appendChild(ul)
+
+ul.style.textAlign = "left"

7db17b931111b4bfc5b2c47ad00847b591ac2838
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-19 19:27:31 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,16 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+
+let element = document.createElement('div');
+document.body.appneChild(element);
+
+let ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appneChild(li);
+}
+
+element.appneChild(ul);
